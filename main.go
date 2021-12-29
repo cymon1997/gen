eae9ba0af425a5d80abd56321f64c29beef2b7de
cymon1997 <cybermonster82@yahoo.com>
2021-12-29 23:23:22 +0700

    update version

--- main.go
+++ main.go
@@ -100,9 +100,9 @@ var (
 func init() {
 	// Setup goopts
 	goopt.Description = func() string {
-		return "ORM and RESTful API generator for SQl databases"
+		return "ORM and RESTful API generator for SQl databases (edited by cymon1997)"
 	}
-	goopt.Version = "v0.9.27 (08/04/2020)"
+	goopt.Version = "v1.0 (29/12/2021)"
 	goopt.Summary = `gen [-v] --sqltype=mysql --connstr "user:password@/dbname" --database <databaseName> --module=example.com/example [--json] [--gorm] [--guregu] [--generate-dao] [--generate-proj]
 git fetch up
            sqltype - sql database type such as [ mysql, mssql, postgres, sqlite, etc. ]
